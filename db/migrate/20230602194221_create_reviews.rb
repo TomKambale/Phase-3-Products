a1fc6ddc479f94e8ebcea01484a5e720044a5f9f
Thomas <thomaskambale1@gmail.com>
2023-06-04 14:08:51 +0300

    r

--- db/migrate/20230602194221_create_reviews.rb
+++ db/migrate/20230602194221_create_reviews.rb
@@ -1,9 +1,11 @@
 class CreateReviews < ActiveRecord::Migration[6.1]
   def change
     create_table :reviews do |t|
-      t.integer :score
+      t.integer :rating
       t.string :comment
-      t.integer :products_id 
+      t.integer :products_id
+      t.integer :user_id 
+      t.timestamps
     end
   end
 end
--- db/migrate/20230602194741_create_products.rb
+++ db/migrate/20230602194741_create_products.rb
@@ -1,10 +1,8 @@
 class CreateProducts < ActiveRecord::Migration[6.1]
   def change
     create_table :products do |t|
-      t.string :title
+      t.string :name
       t.string :genre
-      t.string :platform
-      t.integer :price
       t.timestamps
     end
   end
